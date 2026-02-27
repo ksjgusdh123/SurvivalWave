90c3908834ae31f436bf36c29cbc782ef80f41e8
ksjgusdh123 <122209567+ksjgusdh123@users.noreply.github.com>
2026-02-27 15:39:44 +0900

    박스 스폰 위치 조정

--- SurvivalWave/Assets/Script/Monster/Boss/BossBase.cs
+++ SurvivalWave/Assets/Script/Monster/Boss/BossBase.cs
@@ -2,12 +2,30 @@ using UnityEngine;
 
 public class BossBase : MonsterBase
 {
+    float rayStartHeight = 5f;
+    float rayLength = 10f;
+    LayerMask groundMask;
+    private void Awake()
+    {
+        groundMask = LayerMask.GetMask("Ground");
+    }
     protected override void EndDieAnimation()
     {
         Vector3 spawnPos = transform.position + new Vector3(0f, 1f, 0f);
 
         GameObject go = ItemPool.GetInstance().PopObject(ItemType.Box);
-        go.transform.position = spawnPos;
+        Vector3 rayStart = spawnPos + Vector3.up * rayStartHeight;
+
+        if (Physics.Raycast(rayStart, Vector3.down, out RaycastHit hit, rayStartHeight + rayLength, groundMask, QueryTriggerInteraction.Ignore))
+        {
+            go.transform.position = hit.point;
+        }
+        else
+        {
+            go.transform.position = spawnPos;
+        }
+
+
         MonsterPool.GetInstance().ReturnObject(gameObject, type);
     }
 }
